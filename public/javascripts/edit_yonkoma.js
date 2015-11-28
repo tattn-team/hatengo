827b8c96d0d9117656c52d8b2c53791cfc06944b
Tatsuya Tanaka <tatsuyars@yahoo.co.jp>
2015-11-28 19:41:35 +0900

    Update

--- public/javascripts/edit_yonkoma.js
+++ public/javascripts/edit_yonkoma.js
@@ -27,24 +27,26 @@ $(function() {
 		var results = [];
 
 		$(".yonkoma-box").each(function(i) {
-			var image = $($(this).find(".img-yonkoma")[0]);
+			var image = $($($(this).find(".img-yonkoma")[0])[0]);
 			var image_x = image.position().left; // -15
 			var image_y = image.position().top;
 			console.log(image_x);
 			var serif = $($(this).find(".serif")[0]).text();
 			// results.push({ image_url: image_url, serif: serif});
-			result = {}
-			result["i_url"+i] = image.src;
-			result["p_url"+i] = image.src; // 
-			result["i_x"+i] = image_x;
-			result["i_y"+i] = image_y;
-			result["i_w"+i] = image.width();
-			result["i_h"+i] = image.height();
-			result["serif"+i] = serif;
-			result["s_x"+i] = 0;
-			result["s_y"+i] = 0;
-			result["s_w"+i] = 0;
-			result["s_h"+i] = 0;
+			result = {};
+			no = i + 1;
+			result["i_url"+no] = image.attr('src');
+			result["p_url"+no] = image.attr('src'); // 
+			result["i_x"+no] = image_x;
+			result["i_y"+no] = image_y;
+			result["i_w"+no] = image.width();
+			result["i_h"+no] = image.height();
+			result["serif"+no] = serif;
+			result["s_x"+no] = 0;
+			result["s_y"+no] = 0;
+			result["s_w"+no] = 0;
+			result["s_h"+no] = 0;
+			console.log(result);
 			results.push(result);
 		});
 
